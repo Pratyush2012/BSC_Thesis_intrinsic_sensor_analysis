77e60569d6ec53124aeb3b5d54a039f8e612de94
Pratyush <basnet.pratyush@gmail.com>
2026-03-01 21:30:10 +0100

    creating windowing framework

---
+++ src/windowing.py
@@ -0,0 +1,10 @@
+def detect_stationary_segments(gyro, acc, tcol = "t_rel", gyro_threshold=0.1, acc_threshold=0.1, window_size=5):
+    return
+
+def remove_stationary(): return
+
+def create_windows(): return
+
+def assign_window_labels(): return
+
+def save_windowed_run(): return
